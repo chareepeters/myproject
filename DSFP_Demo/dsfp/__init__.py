3b53dd997812f3bef83ecc6b25a0eb931fde9ad7
Charee Peters <charee.peters@gmail.com>
2016-08-01 17:02:10 -0500

    Added dsfp Demo

---
+++ DSFP_Demo/dsfp/__init__.py
@@ -0,0 +1,26 @@
+"""Tools from DSFP Demo"""
+import astropy
+import astroquery
+
+from astroquery.sdss import SDSS
+def get_photometry(N=10000):
+    """Get photometry from teh SDSS Database
+    
+    Parameters
+    ----------
+    N : int
+        specifies how many 
+    """
+    
+    
+    query = """
+    SELECT TOP {N} 
+    p.psfMag_r, p.fiberMag_r, p.fiber2Mag_r, p.petroMag_r, 
+    p.deVMag_r, p.expMag_r, p.modelMag_r, p.cModelMag_r, 
+    s.class
+    FROM PhotoObjAll AS p JOIN specObjAll s ON s.bestobjid = p.objid
+    WHERE p.mode = 1 AND s.sciencePrimary = 1 AND p.clean = 1 AND s.class != 'QSO'
+    ORDER BY p.objid ASC
+    """
+    data = SDSS.query_sql(query.format(N=N))
+    return data
